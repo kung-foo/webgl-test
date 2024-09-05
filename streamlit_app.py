09689b1df474cf501d722e01ce5bd4978c66803a
Jonathan Camp <github@jonathan.camp>
2024-09-05 13:56:44 +0000

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("WebGL Test")
+
+col1, col2 = st.columns(2)
+
+points = col1.number_input("Points", 1000, 10000)
+
+render_mode = col2.selectbox("Mode", ["svg", "webgl"])
+
+np.random.seed(points) # heh
+
+plot_df = pd.DataFrame.from_dict({"data": np.random.random(points)})
+
+fig = px.line(plot_df, render_mode=render_mode)
+
+st.plotly_chart(fig, use_container_width=True)
